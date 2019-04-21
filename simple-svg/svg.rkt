c17e60d230bae3d1078e206444154ddb9792aa8b
simmone <chenxiao770117@gmail.com>
2019-04-21 10:02:21 +0800

    update

--- simple-svg/svg.rkt
+++ simple-svg/svg.rkt
@@ -1,17 +1,34 @@
 #lang racket
 
 (provide (contract-out
+          [with-output-to-svg (->* (natural? natural? output-port? procedure?)
+                                   (
+                                    #:viewBoxX natural?
+                                    #:viewBoxY natural?
+                                    #:viewBoxWidth natural?
+                                    #:viewBoxHeight natural?
+                                    )
+                                   void?)]
           [*svg* parameter?]
-          [start-svg (-> void?)]
-          [end-svg (-> void?)]
           ))
 
 (define *svg* (make-parameter #f))
 
-(define (start-svg)
-  (fprintf (*svg*) "<svg>\n"))
+(define (with-output-to-svg width height output_port write_proc
+                            #:viewBoxX [viewBoxX 0]
+                            #:viewBoxY [viewBoxY 0]
+                            #:viewBoxWidth [viewBoxWidth width]
+                            #:viewBoxHeight [viewBoxHeight height]
+                            )
+  (parameterize 
+   ([*svg* output_port])
+   (dynamic-wind
+       (lambda () 
+           (fprintf (*svg*) "<svg>\n"))
+         )
+       (lambda () (write_proc))
+       (lambda ()
+         (fprintf (*svg*) "</svg>\n")))
 
-(define (end-svg)
-  (fprintf (*svg*) "</svg>\n"))
 
   
