1bc7c10e417fc789e76af7c1949729ce89416720
Chen Xiao <chenxiao770117@gmail.com>
2019-04-23 12:29:03 +0800

    update

--- simple-svg/main.rkt
+++ simple-svg/main.rkt
@@ -1,15 +1,23 @@
 #lang racket
 
 (require "svg.rkt")
+(require "shapes/rect.rkt")
 
 (provide (contract-out
           [with-output-to-svg (->* (output-port? procedure?)
-                                   (#:width natural?
-                                    #:height natural?
-                                    #:viewBoxX natural?
-                                    #:viewBoxY natural?
-                                    #:viewBoxWidth natural?
-                                    #:viewBoxHeight natural?
+                                   (#:width? natural?
+                                    #:height? natural?
+                                    #:viewBoxX? natural?
+                                    #:viewBoxY? natural?
+                                    #:viewBoxWidth? natural?
+                                    #:viewBoxHeight? natural?
                                     )
                                    void?)]
+          [rect (->* 
+                 (natural? natural? string?)
+                 (
+                  #:start_point pair?
+                  #:radius pair?
+                  )
+                 void?)]
           ))
--- simple-svg/svg.rkt
+++ simple-svg/svg.rkt
@@ -2,12 +2,12 @@
 
 (provide (contract-out
           [with-output-to-svg (->* (output-port? procedure?)
-                                   (#:width natural?
-                                    #:height natural?
-                                    #:viewBoxX natural?
-                                    #:viewBoxY natural?
-                                    #:viewBoxWidth natural?
-                                    #:viewBoxHeight natural?
+                                   (#:width? natural?
+                                    #:height? natural?
+                                    #:viewBoxX? natural?
+                                    #:viewBoxY? natural?
+                                    #:viewBoxWidth? natural?
+                                    #:viewBoxHeight? natural?
                                     )
                                    void?)]
           [*svg* parameter?]
@@ -16,12 +16,12 @@
 (define *svg* (make-parameter #f))
 
 (define (with-output-to-svg output_port write_proc
-                            #:width [width 300]
-                            #:height [height 300]
-                            #:viewBoxX [viewBoxX 0]
-                            #:viewBoxY [viewBoxY 0]
-                            #:viewBoxWidth [viewBoxWidth width]
-                            #:viewBoxHeight [viewBoxHeight height]
+                            #:width? [width? 300]
+                            #:height? [height? 300]
+                            #:viewBoxX? [viewBoxX? 0]
+                            #:viewBoxY? [viewBoxY? 0]
+                            #:viewBoxWidth? [viewBoxWidth? width?]
+                            #:viewBoxHeight? [viewBoxHeight? height?]
                             )
   (parameterize 
    ([*svg* output_port])
@@ -32,15 +32,15 @@
           "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" ~a>\n"
           (with-output-to-string
             (lambda ()
-              (printf "width=\"~a\" height=\"~a\"" width height)
+              (printf "width=\"~a\" height=\"~a\"" width? height?)
               
               (when 
                (or
-                (not (= viewBoxX 0))
-                (not (= viewBoxY 0))
-                (not (= viewBoxWidth width))
-                (not (= viewBoxHeight height)))
-               (printf " viewBox=\"~a ~a ~a ~a\"" viewBoxX viewBoxY viewBoxWidth viewBoxHeight))))))
+                (not (= viewBoxX? 0))
+                (not (= viewBoxY? 0))
+                (not (= viewBoxWidth? width?))
+                (not (= viewBoxHeight? height?)))
+               (printf " viewBox=\"~a ~a ~a ~a\"" viewBoxX? viewBoxY? viewBoxWidth? viewBoxHeight?))))))
        (lambda () (write_proc))
        (lambda ()
          (fprintf (*svg*) "</svg>\n")))))
