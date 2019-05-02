9e2105c1e949e02ebceac3dfa308732ecbcf8f10
Chen Xiao <chenxiao770117@gmail.com>
2019-05-02 11:29:11 +0800

    update

--- simple-svg/tests/path/cubic-path-test.rkt
+++ simple-svg/tests/path/cubic-path-test.rkt
@@ -16,7 +16,7 @@
    (test-case
     "test-basic"
 
-    (call-with-input-file raw_path_svg
+    (call-with-input-file cubic_path_svg
       (lambda (expected)
         (call-with-input-string
          (call-with-output-string
@@ -25,20 +25,15 @@
              output
              #:canvas? '(1 "red" "white")
              (lambda ()
-               (raw-path
-                240 166
-                "M248.761,92c0,9.801-7.93,17.731-17.71,17.731c-0.319,0-0.617,0-0.935-0.021
-           c-10.035,37.291-51.174,65.206-100.414,65.206 c-49.261,0-90.443-27.979-100.435-65.334
-           c-0.765,0.106-1.531,0.149-2.317,0.149c-9.78,0-17.71-7.93-17.71-17.731
-           c0-9.78,7.93-17.71,17.71-17.71c0.787,0,1.552,0.042,2.317,0.149
-           C39.238,37.084,80.419,9.083,129.702,9.083c49.24,0,90.379,27.937,100.414,65.228h0.021
-           c0.298-0.021,0.617-0.021,0.914-0.021C240.831,74.29,248.761,82.22,248.761,92z"
-                #:fill? "#7AA20D"
-                #:stroke-fill? "#7AA20D"
-                #:stroke-width? 9
-                #:stroke-linejoin? "round")))))
+               (path
+                #:stroke-fill? "#333333"
+                #:stroke-width? 3
+                (lambda ()
+                  (moveto* '(10 . 50))
+                  (cubic-curve* '(30 . 5) '(80 . 5) '(100 . 50))
+                  (cubic-curve* '(120 . 95) '(170 . 95) '(190 . 50)))))))
          (lambda (actual)
-           (check-lines? expected actual))))))
+           (check-lines? expected actual)))))))
 
    ))
 
