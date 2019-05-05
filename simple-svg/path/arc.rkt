bc623fd70d04b35e10091cc10d8889928c06b8c9
Chen Xiao <chenxiao770117@gmail.com>
2019-05-05 13:23:12 +0800

    update

---
+++ simple-svg/path/arc.rkt
@@ -0,0 +1,69 @@
+#lang racket
+
+(require "../svg.rkt")
+(require "path.rkt")
+
+(provide (contract-out
+          [arc (->
+                natural?
+                natural?
+                (cons/c integer? integer?)
+                (list/c 'left_big 'left_small 'right_big 'right_small)
+                (cons/c integer? integer?)
+                void?)]
+          [arc* (->
+                 natural?
+                 natural?
+                 (cons/c integer? integer?)
+                 (list/c 'left_big 'left_small 'right_big 'right_small)
+                 (cons/c integer? integer?)
+                 void?)]
+          ))
+
+(define (arc point1 point2 point3) (curve 'c point1 point2 point3))
+
+(define (arc* point1 point2 point3) (curve 'C point1 point2 point3))
+
+(define (curve type point1 point2 point3)
+  ((*sequence-set*))
+
+  (let ([point1* #f]
+        [point2* #f]
+        [point3* #f])
+  (if (eq? type 'C)
+      (begin
+        (set! point1* point1)
+        (set! point2* point2)
+        (set! point3* point3))
+      (begin
+        (set! point1*
+              (cons
+               (+ (car point1) (car ((*position-get*))))
+               (+ (cdr point1) (cdr ((*position-get*))))))
+
+        (set! point2*
+              (cons
+               (+ (car point2) (car ((*position-get*))))
+               (+ (cdr point2) (cdr ((*position-get*))))))
+
+        (set! point3*
+              (cons
+               (+ (car point3) (car ((*position-get*))))
+               (+ (cdr point3) (cdr ((*position-get*))))))))
+        
+      ((*position-set*) point3*)
+
+      ((*size-func*) (car point1*) (cdr point1*))
+      ((*size-func*) (car point2*) (cdr point2*))
+      ((*size-func*) (car point3*) (cdr point3*))
+
+      (if (eq? type 'C)
+          (fprintf (*svg*) "           C~a,~a ~a,~a ~a,~a\n"
+                   (+ (car point1*) (*padding*)) (+ (cdr point1*) (*padding*))
+                   (+ (car point2*) (*padding*)) (+ (cdr point2*) (*padding*))
+                   (+ (car point3*) (*padding*)) (+ (cdr point3*) (*padding*)))
+          (fprintf (*svg*) "           c~a,~a ~a,~a ~a,~a\n"
+                   (car point1) (cdr point1)
+                   (car point2) (cdr point2)
+                   (car point3) (cdr point3)))))
+
---
+++ simple-svg/tests/path/arc-test.rkt
@@ -0,0 +1,64 @@
+#lang racket
+
+(require rackunit)
+(require rackunit/text-ui)
+
+(require "../../lib/lib.rkt")
+(require "../../main.rkt")
+
+(require racket/runtime-path)
+(define-runtime-path arc_svg "../../showcase/path/arc.svg")
+
+(define test-all
+  (test-suite
+   "test-path"
+
+   (test-case
+    "test-arc"
+
+    (call-with-input-file arc_svg
+      (lambda (expected)
+        (call-with-input-string
+         (call-with-output-string
+          (lambda (output)
+            (with-output-to-svg
+             output
+             #:canvas? '(1 "red" "white")
+             (lambda ()
+               (path
+                #:stroke-fill? "#ccccff"
+                #:stroke-width? 3
+                (lambda ()
+                  (moveto* '(120 . 35))
+                  (arc* '(80 . 40) 'left_big '(160 . 75))))
+
+               (path
+                #:stroke-fill? "green"
+                #:stroke-width? 3
+                (lambda ()
+                  (moveto* '(120 . 35))
+                  (arc* '(80 . 40) 'left_small '(160 . 75))))
+
+               (path
+                #:stroke-fill? "blue"
+                #:stroke-width? 3
+                (lambda ()
+                  (moveto* '(120 . 35))
+                  (arc* '(80 . 40) 'right_big '(160 . 75))))
+
+               (path
+                #:stroke-fill? "yellow"
+                #:stroke-width? 3
+                (lambda ()
+                  (moveto* '(120 . 35))
+                  (arc* '(80 . 40) 'right_small '(160 . 75))))
+
+               (circle '(120 . 35) 2 "red")
+               (circle '(160 . 75) 2 "red")
+               ))))
+         (lambda (actual)
+           (check-lines? expected actual))))))
+
+   ))
+
+(run-tests test-all)
