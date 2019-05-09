f88a9340f0fdc9eb7d271cdca6f28f46549edaf0
Chen Xiao <chenxiao770117@gmail.com>
2019-05-09 10:10:09 +0800

    update

--- simple-svg/main.rkt
+++ simple-svg/main.rkt
@@ -4,16 +4,18 @@
 (require "shapes/rect.rkt")
 
 (provide (contract-out
-          [svg-out (->* (output-port? procedure?)
-                                   (#:padding? natural?
-                                    #:width? (or/c #f natural?)
-                                    #:height? (or/c #f natural?)
-                                    #:viewBox? (or/c #f (list/c natural? natural? natural? natural?))
-                                    #:canvas? (or/c #f (list/c natural? string? string?))
-                                    )
-                                   void?)]
+          [svg-out (->* (procedure?)
+                        (
+                         #:padding? natural?
+                         #:width? (or/c #f natural?)
+                         #:height? (or/c #f natural?)
+                         #:viewBox? (or/c #f (list/c natural? natural? natural? natural?))
+                         #:canvas? (or/c #f (list/c natural? string? string?))
+                         )
+                        string?)]
           [svg-set-property (-> string? symbol? any/c void?)]
           [svg-use (->* (string?) (#:at? (cons/c natural? natural?)) void?)]
+          [svg-show-default (-> void?)]
           [svg-rect-def (->* 
                  (natural? natural?)
                  (
--- simple-svg/svg.rkt
+++ simple-svg/svg.rkt
@@ -1,16 +1,18 @@
 #lang racket
 
 (provide (contract-out
-          [svg-out (->* (output-port? procedure?)
-                                   (#:padding? natural?
-                                    #:width? (or/c #f natural?)
-                                    #:height? (or/c #f natural?)
-                                    #:viewBox? (or/c #f (list/c natural? natural? natural? natural?))
-                                    #:canvas? (or/c #f (list/c natural? string? string?))
-                                    )
-                                   string?)]
+          [svg-out (->* (procedure?)
+                        (
+                         #:padding? natural?
+                         #:width? (or/c #f natural?)
+                         #:height? (or/c #f natural?)
+                         #:viewBox? (or/c #f (list/c natural? natural? natural? natural?))
+                         #:canvas? (or/c #f (list/c natural? string? string?))
+                         )
+                        string?)]
           [svg-set-property (-> string? symbol? any/c void?)]
           [svg-use (->* (string?) (#:at? (cons/c natural? natural?)) void?)]
+          [svg-show-default (-> void?)]
           [*shape-index* parameter?]
           [*add-shape* parameter?]
           ))
@@ -21,85 +23,98 @@
 (define *add-shape* (make-parameter #f))
 (define *set-property* (make-parameter #f))
 (define *add-group* (make-parameter #f))
+(define *add-to-show* (make-parameter #f))
+(define *groups_map* (make-parameter #f))
 (define *size-func* (make-parameter #f))
 (define *current_group* (make-parameter #f))
 
-(define (svg-out output_port write_proc
-                            #:padding? [padding? 10]
-                            #:width? [width? #f]
-                            #:height? [height? #f]
-                            #:viewBox? [viewBox? #f]
-                            #:canvas? [canvas? #f]
-                            )
-  (parameterize 
-   ([*svg* output_port])
-   (dynamic-wind
-       (lambda () 
-         (fprintf 
-          (*svg*)
-          "<svg\n    ~a\n    ~a\n    ~a\n"
-          "version=\"1.1\""
-          "xmlns=\"http://www.w3.org/2000/svg\""
-          "xmlns:xlink=\"http://www.w3.org/1999/xlink\"\n")
-       (lambda ()
-         (let* ([max_width 0]
-                [max_height 0]
-                [shapes_count 0]
-                [groups_count 0]
-                [shapes_map (make-hash)]
-                [groups_map (make-hash)]
-                [content
-                 (call-with-output-string
-                  (lambda (svg_output_port)
-                    (parameterize
-                        (
-                         [*svg* svg_output_port]
-                         [*shape-index* (lambda () (set! shapes_count (add1 shapes_count)) shapes_count)]
-                         [*group-index* (lambda () (set! groups_count (add1 groups_count)) groups_count)]
-                         [*size-func* 
-                          (lambda (_width _height)
-                            (when (> _width max_width) (set! max_width _width))
-                            (when (> _height max_height) (set! max_height _height)))]
-                         [*add-shape*
-                          (lambda (_index shape)
-                            (hash-set! shapes_map _index shape))]
-                         [*add-group*
-                          (lambda (_index xy)
-                            (hash-set! groups_map
-                                       (*current_group*)
-                                       `(,@(hash-ref groups_map (*current_group*) '())
-                                         ,(list _index xy))))]
-                         [*set-property*
-                          (lambda (_index property value)
-                            (let ([property_map (second (hash-ref shapes_map _index))])
-                              (hash-set! property_map property value)))]
-                         [*current_group* "default"]
-                         )
-                      (write_proc))))])
-           
-           (fprintf (*svg*) "    width=\"~a\" height=\"~a\"\n"
-                    (if width? width? (+ max_width (* padding? 2)))
-                    (if height? height? (+ max_height (* padding? 2))))
+(define (svg-out write_proc
+                 #:padding? [padding? 10]
+                 #:width? [width? #f]
+                 #:height? [height? #f]
+                 #:viewBox? [viewBox? #f]
+                 #:canvas? [canvas? #f]
+                 )
+  (with-output-to-string
+   (lambda ()
+     (dynamic-wind
+         (lambda () 
+           (printf 
+            "<svg\n    ~a\n    ~a\n    ~a\n"
+            "version=\"1.1\""
+            "xmlns=\"http://www.w3.org/2000/svg\""
+            "xmlns:xlink=\"http://www.w3.org/1999/xlink\"\n"))
+         (lambda ()
+           (let* ([max_width 0]
+                  [max_height 0]
+                  [shapes_count 0]
+                  [groups_count 0]
+                  [shapes_map (make-hash)]
+                  [groups_map (make-hash)]
+                  [show_list '()]
+                  [content
+                   (call-with-output-string
+                    (lambda (svg_output_port)
+                      (parameterize
+                       (
+                        [*shape-index* (lambda () (set! shapes_count (add1 shapes_count)) shapes_count)]
+                        [*group-index* (lambda () (set! groups_count (add1 groups_count)) groups_count)]
+                        [*size-func* 
+                         (lambda (_width _height)
+                           (when (> _width max_width) (set! max_width _width))
+                           (when (> _height max_height) (set! max_height _height)))]
+                        [*add-shape*
+                         (lambda (_index shape)
+                           (hash-set! shapes_map _index shape))]
+                        [*groups_map* (make-hash)]
+                        [*add-group*
+                         (lambda (_index at)
+                           (hash-set! (*groups_map*)
+                                      (*current_group*)
+                                      `(,@(hash-ref groups_map (*current_group*) '())
+                                        ,(list _index at))))]
+                        [*add-to-show* (lambda (group_index) (set! show_list `(,@show_list ,group_index)))]
+                        [*set-property*
+                         (lambda (_index property value)
+                           (let ([property_map (second (hash-ref shapes_map _index))])
+                             (hash-set! property_map property value)))]
+                        [*current_group* "default"]
+                        )
+                       (write_proc))))])
+             
+             (printf "    width=\"~a\" height=\"~a\"\n"
+                     (if width? width? (+ max_width (* padding? 2)))
+                     (if height? height? (+ max_height (* padding? 2))))
 
-           (when viewBox?
-                 (fprintf (*svg*) "    viewBox=\"~a ~a ~a ~a\"\n"
-                          (first viewBox?) (second viewBox?) (third viewBox?) (fourth viewBox?)))
-           
-           (fprintf (*svg*) "    >\n")
-         
-           (when canvas?
-                 (fprintf (*svg*) "  <rect width=\"~a\" height=\"~a\" stroke-width=\"~a\" stroke=\"~a\" fill=\"~a\" />\n"
-                          (if width? width? (+ max_width (* padding? 2)))
-                          (if height? height? (+ max_height (* padding? 2)))
-                    (first canvas?) (second canvas?) (third canvas?)))
-          
-           (fprintf (*svg*) "~a" content)))
-       (lambda ()
-         (fprintf (*svg*) "</svg>\n"))))))
+             (when viewBox?
+                   (printf "    viewBox=\"~a ~a ~a ~a\"\n"
+                           (first viewBox?) (second viewBox?) (third viewBox?) (fourth viewBox?)))
+             
+             (printf "    >\n")
+             
+             (when canvas?
+                   (printf "  <rect width=\"~a\" height=\"~a\" stroke-width=\"~a\" stroke=\"~a\" fill=\"~a\" />\n"
+                           (if width? width? (+ max_width (* padding? 2)))
+                           (if height? height? (+ max_height (* padding? 2)))
+                           (first canvas?) (second canvas?) (third canvas?)))
+             
+             (printf "~a" content)))
+         (lambda ()
+           (flush-data)
+           (printf "</svg>\n"))))))
 
 (define (svg-set-property index property value)
   ((*set-property*) index property value))
 
-
 (define (svg-use index #:at? [at? '(0 . 0)])
   ((*add-group*) index at?))
+
+(define (svg-show-default)
+  (svg-show "default"))
+
+(define (svg-show group_index)
+  ((*add-to-show*) group_index))
+
+(define (flush-data)
+  (void)
+  )
