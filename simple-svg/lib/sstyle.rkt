7af179bc79f38f751b09b5b26b038061fdc991d7
Chen Xiao <chenxiao770117@gmail.com>
2019-05-29 17:47:26 +0800

    update

--- simple-svg/lib/sstyle.rkt
+++ simple-svg/lib/sstyle.rkt
@@ -9,15 +9,37 @@
           [sstyle-clone (-> sstyle/c sstyle/c)]
           ))
 
-(struct sstyle (fill stroke stroke-width stroke-linejoin translate rotate scale skewX skewY) #:transparent #:mutable)
+(struct sstyle (
+                fill
+                fill-rule
+                fill-opacity
+                stroke
+                stroke-width
+                stroke-linecap
+                stroke-linejoin
+                stroke-miterlimit
+                stroke-dasharray
+                stroke-dashoffset
+                translate
+                rotate
+                scale
+                skewX
+                skewY
+                ) #:transparent #:mutable)
 
 (define sstyle/c
   (struct/dc
    sstyle
      [fill string?]
+     [fill-rule (or/c #f 'nonzero 'evenodd 'inerit)]
+     [fill-opacity (or/c #f (between/c 0 1))]
      [stroke (or/c #f string?)]
      [stroke-width (or/c #f natural?)]
+     [stroke-linecap (or/c #f 'butt 'round 'square 'inherit)]
      [stroke-linejoin (or/c #f 'miter 'round 'bevel)]
+     [stroke-miterlimit (or/c #f (>=/c 1))]
+     [stroke-dasharray (or/c #f string?)]
+     [stroke-dashoffset (or/c #f natural?)]
      [translate (or/c #f (cons/c natural? natural?))]
      [rotate (or/c #f integer?)]
      [scale (or/c #f natural? (cons/c natural? natural?))]
@@ -28,9 +50,15 @@
 (define (sstyle-clone sv)
   (sstyle
    (sstyle-fill sv)
+   (sstyle-fill-rule sv)
+   (sstyle-fill-opacity sv)
    (sstyle-stroke sv)
    (sstyle-stroke-width sv)
+   (sstyle-stroke-linecap sv)
    (sstyle-stroke-linejoin sv)
+   (sstyle-stroke-miterlimit sv)
+   (sstyle-stroke-dasharray sv)
+   (sstyle-stroke-dashoffset sv)
    (sstyle-translate sv)
    (sstyle-rotate sv)
    (sstyle-scale sv)
@@ -42,11 +70,23 @@
   (sstyle
 ;; fill color
    "none"
+;; fill-rule
+   #f
+;; fill-opacity
+   #f
 ;; stroke color
    #f
 ;; stroke width
    #f
+;; stroke-linecap
+   #f
 ;; stroke-linejoin
+   #f
+;; stroke-miterlimit
+   #f
+;; stroke-dasharray
+   #f
+;; stroke-dashoffset
    #f
 ;; translate
    #f
@@ -65,6 +105,12 @@
     (lambda ()
       (printf "fill=\"~a\" " (sstyle-fill _sstyle))
 
+      (when (sstyle-fill-rule _sstyle)
+            (printf "fill-rule=\"~a\" " (sstyle-fill-rule _sstyle)))
+
+      (when (sstyle-fill-opacity _sstyle)
+            (printf "fill-opacity=\"~a\" " (sstyle-fill-opacity _sstyle)))
+
       (when (sstyle-stroke-width _sstyle)
             (printf "stroke-width=\"~a\" " (sstyle-stroke-width _sstyle))
 
@@ -72,7 +118,20 @@
                   (printf "stroke=\"~a\" " (sstyle-stroke _sstyle)))
 
             (when (sstyle-stroke-linejoin _sstyle)
-                  (printf "stroke-linejoin=\"~a\" " (sstyle-stroke-linejoin _sstyle))))
+                  (printf "stroke-linejoin=\"~a\" " (sstyle-stroke-linejoin _sstyle)))
+
+            (when (sstyle-stroke-linecap _sstyle)
+                  (printf "stroke-linecap=\"~a\" " (sstyle-stroke-linecap _sstyle)))
+
+            (when (sstyle-stroke-miterlimit _sstyle)
+                  (printf "stroke-miterlimit=\"~a\" " (sstyle-stroke-miterlimit _sstyle)))
+
+            (when (sstyle-stroke-dasharray _sstyle)
+                  (printf "stroke-dasharray=\"~a\" " (sstyle-stroke-dasharray _sstyle)))
+
+            (when (sstyle-stroke-dashoffset _sstyle)
+                  (printf "stroke-dashoffset=\"~a\" " (sstyle-stroke-dashoffset _sstyle)))
+            )
       
       (when (or
              (sstyle-translate _sstyle)
--- simple-svg/scribble/sstyle.scrbl
+++ simple-svg/scribble/sstyle.scrbl
@@ -14,10 +14,22 @@ sstyle is a struct, it represent a shape or group's style.
 svg-use-shape and svg-show-group should use the sstyle.
 
 @defstruct*[sstyle (
-              [fill string?]
-              [stroke (or/c #f string?)]
-              [stroke-width (or/c #f natural?)]
-              [stroke-linejoin (or/c #f 'miter 'round 'bevel)])]{
+     [fill string?]
+     [fill-rule (or/c #f 'nonzero 'evenodd 'inerit)]
+     [fill-opacity (or/c #f (between/c 0 1))]
+     [stroke (or/c #f string?)]
+     [stroke-width (or/c #f natural?)]
+     [stroke-linecap (or/c #f 'butt 'round 'square 'inherit)]
+     [stroke-linejoin (or/c #f 'miter 'round 'bevel)]
+     [stroke-miterlimit (or/c #f (>=/c 1))]
+     [stroke-dasharray (or/c #f string?)]
+     [stroke-dashoffset (or/c #f natural?)]
+     [translate (or/c #f (cons/c natural? natural?))]
+     [rotate (or/c #f integer?)]
+     [scale (or/c #f natural? (cons/c natural? natural?))]
+     [skewX (or/c #f natural?)]
+     [skewY (or/c #f natural?)]
+     )]{
 }
 
 @defproc[(sstyle-new) sstyle/c]{
