a58292b70661453f544834ad8ddf1e842bbaa49b
Chen Xiao <chenxiao770117@gmail.com>
2019-06-07 13:36:15 +0800

    update

---
+++ info.rkt
@@ -0,0 +1,11 @@
+#lang setup/infotab
+
+(define version "1.0")
+
+(define collection 'multi)
+
+(define deps '("base"
+               "rackunit-lib"
+               "racket-doc"
+               "scribble-lib"
+               ))
--- simple-svg/info.rkt
+++ simple-svg/info.rkt
@@ -2,7 +2,7 @@
 (define scribblings
   '(("scribble/simple-svg.scrbl" (multi-page) (tool 100))))
 
-(define compile-omit-paths '("tests" "docs"))
-(define test-omit-paths '("tests" "docs"))
+(define compile-omit-paths '("tests" "refs"))
+(define test-omit-paths '("tests" "refs"))
 
 
