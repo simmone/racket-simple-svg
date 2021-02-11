662ca9c1352ec3136270fc7b328a4d2b59d1dede
Chen Xiao <chenxiao770117@gmail.com>
2021-02-11 22:25:05 +0800

    update

--- simple-svg/info.rkt
+++ simple-svg/info.rkt
@@ -3,6 +3,6 @@
   '(("scribble/simple-svg.scrbl" (multi-page))))
 
 (define compile-omit-paths '("tests" "refs"))
-(define test-omit-paths '("tests" "refs"))
+(define test-include-paths '("tests"))
 
 
