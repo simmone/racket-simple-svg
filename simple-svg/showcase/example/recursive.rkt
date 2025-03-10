51934f493effb81ce177ea0c65517ca6d8fbdd0d
Chen Xiao <chenxiao770117@gmail.com>
2025-03-10 20:16:31 +0800

    update

--- simple-svg/showcase/example/recursive.rkt
+++ simple-svg/showcase/example/recursive.rkt
@@ -10,18 +10,26 @@
                 (svg-out
                  canvas_size canvas_size
                  (lambda ()
-                   (let ([_sstyle (sstyle-new)])
+                   (let ([radius_to_circle_id_map (make-hash)]
+                         [origin_radius 100]
+                         [_sstyle (sstyle-new)])
+                     
+                     (let loop-radius ([loop_radius origin_radius])
+                       (when (> loop_radius 6)
+                         (hash-set! radius_to_circle_id_map loop_radius (svg-def-shape (new-circle loop_radius)))
+                         (loop-radius (/ loop_radius 2))))
+                     
                      (set-SSTYLE-stroke! _sstyle "red")
                      (set-SSTYLE-stroke-width! _sstyle 1)
 
-                     (letrec ([recur-circle 
-                               (lambda (x y radius)
-                                 (let ([circle_id (svg-def-shape (new-circle radius))])
-                                   (svg-place-widget circle_id #:style _sstyle #:at (cons x y)))
+                     (let loop ([loop_x 200]
+                                [loop_y 200]
+                                [loop_radius origin_radius])
+                       (when (> loop_radius 6)
+                         (let ([circle_id (hash-ref radius_to_circle_id_map loop_radius)])
+                           (svg-place-widget circle_id #:style _sstyle #:at (cons loop_x loop_y)))
 
-                                 (when (> radius 8)
-                                   (recur-circle (+ x radius) y (/ radius 2))
-                                   (recur-circle (- x radius) y (/ radius 2))
-                                   (recur-circle x (+ y radius) (/ radius 2))
-                                   (recur-circle x (- y radius) (/ radius 2))))])
-                       (recur-circle 200 200 100)))))))))
+                         (loop (+ loop_x loop_radius) loop_y (/ loop_radius 2))
+                         (loop (- loop_x loop_radius) loop_y (/ loop_radius 2))
+                         (loop loop_x (+ loop_y loop_radius) (/ loop_radius 2))
+                         (loop loop_x (- loop_y loop_radius) (/ loop_radius 2)))))))))))
