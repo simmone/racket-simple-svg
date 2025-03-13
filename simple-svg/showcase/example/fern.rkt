366544f682255d7778aab2baae9661f1adfb2dba
Chen Xiao <chenxiao770117@gmail.com>
2025-03-13 09:42:07 +0800

    update

--- simple-svg/showcase/example/fern.rkt
+++ simple-svg/showcase/example/fern.rkt
@@ -5,7 +5,7 @@
 ;;; calculate the end point
 (define (get-end-point start_point #:length length #:deg deg #:precision precision)
   (let* ([end (make-polar length (* 2 pi (/ deg 360)))]
-         [end_x (string->number (~r #:precision precision (+ (car  start_point) (real-part end))))]
+         [end_x (string->number (~r #:precision precision (+ (car start_point) (real-part end))))]
          [end_y (string->number (~r #:precision precision (+ (cdr start_point) (imag-part end))))])
     (cons end_x end_y)))
 
