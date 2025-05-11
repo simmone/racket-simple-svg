35cd67938cee76b4edc11a4bf56e85c8f54daaf4
Chen Xiao <chenxiao770117@gmail.com>
2025-05-12 03:22:04 +0800

    update

--- simple-svg/tests/showcase/recursive-test.rkt
+++ simple-svg/tests/showcase/recursive-test.rkt
@@ -19,27 +19,26 @@
            (svg-out
             400 400
             (lambda ()
-              (let ([origin_radius 100]
-                    [_sstyle (sstyle-new)])
-                
+              (let ([_sstyle (sstyle-new)]
+                    [top_group_id
+                     (svg-def-group
+                      (lambda ()
+                        (let loop ([loop_x 200]
+                                   [loop_y 200]
+                                   [loop_radius 100])
+                          (when (> loop_radius 6)
+                            (let ([circle_id (svg-def-shape (new-circle loop_radius))])
+                              (svg-place-widget circle_id #:at (cons loop_x loop_y)))
+
+                            (loop (+ loop_x loop_radius) loop_y (/ loop_radius 2))
+                            (loop (- loop_x loop_radius) loop_y (/ loop_radius 2))
+                            (loop loop_x (+ loop_y loop_radius) (/ loop_radius 2))
+                            (loop loop_x (- loop_y loop_radius) (/ loop_radius 2))))))])
+
                 (set-SSTYLE-stroke! _sstyle "red")
                 (set-SSTYLE-stroke-width! _sstyle 1)
-                
-                (let ([top_group_id
-                       (svg-def-group
-                        (lambda ()
-                          (let loop ([loop_x 200]
-                                     [loop_y 200]
-                                     [loop_radius origin_radius])
-                            (when (> loop_radius 6)
-                              (let ([circle_id (svg-def-shape (new-circle loop_radius))])
-                                (svg-place-widget circle_id #:at (cons loop_x loop_y)))
 
-                              (loop (+ loop_x loop_radius) loop_y (/ loop_radius 2))
-                              (loop (- loop_x loop_radius) loop_y (/ loop_radius 2))
-                              (loop loop_x (+ loop_y loop_radius) (/ loop_radius 2))
-                              (loop loop_x (- loop_y loop_radius) (/ loop_radius 2))))))])
-                  (svg-place-widget top_group_id #:style _sstyle)))))])
+                (svg-place-widget top_group_id #:style _sstyle))))])
 
       (call-with-input-file recursive_svg
         (lambda (expected)
