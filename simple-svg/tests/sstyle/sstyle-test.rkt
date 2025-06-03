53a7dcae8e13ef4df07ff017e7cd650cce11046b
Chen Xiao <chenxiao770117@gmail.com>
2025-06-03 11:02:34 +0800

    update

--- simple-svg/tests/sstyle/sstyle-test.rkt
+++ simple-svg/tests/sstyle/sstyle-test.rkt
@@ -3,6 +3,7 @@
 (require rackunit
          rackunit/text-ui
          "../../src/lib.rkt"
+         "../../src/global.rkt"
          "../../src/define/sstyle.rkt"
          racket/runtime-path)
 
@@ -19,7 +20,9 @@
       (set-SSTYLE-fill-opacity! _sstyle 0.5)
 
       (check-equal?
-       (sstyle-format _sstyle)
+       (parameterize
+           ([*PRECISION* 4])
+         (sstyle-format _sstyle))
        " fill=\"url(#s1)\" fill-rule=\"nonzero\" fill-opacity=\"0.5\""
        )))
 
@@ -32,7 +35,9 @@
       (set-SSTYLE-fill-opacity! _sstyle 0.5)
 
       (check-equal?
-       (sstyle-format _sstyle)
+       (parameterize
+           ([*PRECISION* 4])
+         (sstyle-format _sstyle))
        " fill=\"red\" fill-rule=\"nonzero\" fill-opacity=\"0.5\""
        )))
 
@@ -49,7 +54,9 @@
       (set-SSTYLE-stroke-dashoffset! _sstyle 5)
 
       (check-equal?
-       (sstyle-format _sstyle)
+       (parameterize
+           ([*PRECISION* 4])
+         (sstyle-format _sstyle))
        " fill=\"none\" stroke-width=\"5\" stroke=\"#ABABAB\" stroke-linejoin=\"round\" stroke-linecap=\"square\" stroke-miterlimit=\"2\" stroke-dasharray=\"40,10\" stroke-dashoffset=\"5\""
        )))
 
@@ -64,7 +71,9 @@
       (set-SSTYLE-skewY! _sstyle 3)
 
       (check-equal?
-       (sstyle-format _sstyle)
+       (parameterize
+           ([*PRECISION* 4])
+         (sstyle-format _sstyle))
        " fill=\"none\" transform=\"translate(0.1 0.2) rotate(30) scale(1) skewX(2) skewY(3)\""
        )))
 
@@ -75,7 +84,9 @@
       (set-SSTYLE-scale! _sstyle '(2 . 3))
 
       (check-equal?
-       (sstyle-format _sstyle)
+       (parameterize
+           ([*PRECISION* 4])
+         (sstyle-format _sstyle))
        " fill=\"none\" transform=\"scale(2 3)\""
        )))
    ))
